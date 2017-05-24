230fc27c0237cb7c79aa685268840b1b9f54bdbf
Léo Huppert <leo.huppert57@gmail.com>
2017-05-25 01:15:38 +0200

    added Register action

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -2,7 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -20,11 +23,39 @@ class SecurityController extends Controller
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-
-
         return $this->render('security/login.html.twig', array(
             'error'         => $error,
             'last_username' => $lastUsername
+        ));
+    }
+
+    /**
+     * @return Response
+     *
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isValid() && $form->isSubmitted()) {
+            $em = $this->getDoctrine()
+                ->getManager();
+
+            $encoder = $this->get('security.password_encoder');
+
+            $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('security/register.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 }
